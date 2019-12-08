5a80c381af3b1d26eb45b7c0045014f785999bfb
Benbacker <benbacker99er@gmail.com>
2019-12-08 23:07:09 +0700

    sucesss project & database

--- resources/views/customer/layout/bot.blade.php
+++ resources/views/customer/layout/bot.blade.php
@@ -5,8 +5,6 @@
 <script src="{{asset('assets/customer/js/main.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-    crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
         $('#login').click(function (e) { 
